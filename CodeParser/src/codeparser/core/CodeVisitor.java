cf157247912703604937721a28a02bedefbd08c1
s_suzuki <y535039b@mails.cc.ehime-u.ac.jp>
2016-02-24 16:40:36 +0900

    [under development]edit Visitor file
    add modifiers printing function

--- CodeParser/src/codeparser/core/CodeVisitor.java
+++ CodeParser/src/codeparser/core/CodeVisitor.java
@@ -1,12 +1,12 @@
 package codeparser.core;
 
 import java.util.Iterator;
-
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.BodyDeclaration;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.Javadoc;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.Modifier;
 import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 
@@ -24,10 +24,28 @@ class CodeVisitor extends ASTVisitor
 		System.out.println("range "+(unit.getLineNumber(node.getStartPosition())+docRange)+" "
 				+unit.getLineNumber(node.getStartPosition()+node.getLength()));
 	}
+	private static void printModifiers(int flags)
+	{
+		String modifiers="modifier ";
+		modifiers+=Modifier.isAbstract(flags)?"abstract ":"";
+		modifiers+=Modifier.isDefault(flags)?"default ":"";
+		modifiers+=Modifier.isFinal(flags)?"final ":"";
+		modifiers+=Modifier.isNative(flags)?"native ":"";
+		modifiers+=Modifier.isPrivate(flags)?"private ":"";
+		modifiers+=Modifier.isProtected(flags)?"protected ":"";
+		modifiers+=Modifier.isPublic(flags)?"public ":"";
+		modifiers+=Modifier.isStatic(flags)?"static ":"";
+		modifiers+=Modifier.isStrictfp(flags)?"strictfp ":"";
+		modifiers+=Modifier.isSynchronized(flags)?"synchronized ":"";
+		modifiers+=Modifier.isTransient(flags)?"transient ":"";
+		modifiers+=Modifier.isVolatile(flags)?"volatile ":"";
+		System.out.println(modifiers);
+	}
 	private static void printTypeDeclarationState(TypeDeclaration node)
 	{
 		String nodeType=node.isInterface()?"interface ":"class ";
 		System.out.println(nodeType+node.getName());
+		printModifiers(node.getModifiers());
 		printDeclarationRange(node);
 		System.out.println();
 	}
@@ -38,6 +56,7 @@ class CodeVisitor extends ASTVisitor
 		if(!node.isConstructor()){
 			System.out.println("returnType "+node.getReturnType2());
 		}
+		printModifiers(node.getModifiers());
 		System.out.print("arguments ");
 		for(Iterator<SingleVariableDeclaration> iter=node.parameters().iterator();iter.hasNext();){
 			SingleVariableDeclaration argument=iter.next();
